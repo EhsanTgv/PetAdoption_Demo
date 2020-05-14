ad803195ca9a899a971e7eb154b2eacf399caae7
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-14 20:04:37 +0430

    MyHomePage implement appBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
@@ -22,7 +23,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Column()),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.only(top: 40),
+                      padding: EdgeInsets.only(left: 15),
+                      child: Icon(Icons.menu),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Container(),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(top: 40),
+                      padding: EdgeInsets.only(right: 15),
+                      child: Icon(Icons.person),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
