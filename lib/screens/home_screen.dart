aff98c66bde1fb8d57893a584194143c20fb0c06
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-15 22:33:20 +0430

    HomePage implement safe area for top

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,35 +11,36 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      margin: EdgeInsets.only(top: 40),
-                      padding: EdgeInsets.only(left: 15),
-                      child: Icon(Icons.menu),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        padding: EdgeInsets.only(left: 15),
+                        child: Icon(Icons.menu),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 6,
-                    child: Container(),
-                  ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.only(top: 40),
-                      padding: EdgeInsets.only(right: 15),
-                      child: Icon(Icons.person),
+                    Expanded(
+                      flex: 6,
+                      child: Container(),
                     ),
-                  ),
-                ],
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(right: 15),
+                        child: Icon(Icons.person),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
