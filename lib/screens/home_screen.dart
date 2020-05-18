c225ae61f5d50d781967a2a2923e75cdc5617632
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-18 21:52:51 +0430

    HomePage implement Horizontal ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -114,6 +114,35 @@ class _HomePageState extends State<HomePage> {
               appBarWidget(),
               titleWidget(),
               tabBarWidget(),
+              Container(
+      margin: EdgeInsets.symmetric(vertical: 20.0),
+      height: 200.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            width: 160.0,
+            color: Colors.red,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.blue,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.green,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.yellow,
+          ),
+          Container(
+            width: 160.0,
+            color: Colors.orange,
+          ),
+        ],
+      ),
+    ),
             ],
           ),
         ),
