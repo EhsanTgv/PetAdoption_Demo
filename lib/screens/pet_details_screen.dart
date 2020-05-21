dd1abc142c8d569408d72a53cffeafc7a5feb1a8
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-21 22:18:45 +0430

    PetDetailsScreen add some decoration to name row

--- lib/screens/pet_details_screen.dart
+++ lib/screens/pet_details_screen.dart
@@ -13,22 +13,42 @@ class _PetDetailsScreenState extends State<PetDetailsScreen> {
         child: Column(
           children: <Widget>[
             coverImageWidget(),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text("Charlie"),
-                      Text("Pug Dog"),
-                    ],
+            Container(
+              margin: EdgeInsets.only(top: 32, right: 24, left: 24),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Charlie",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Text(
+                          "Pug Dog",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                )
-              ],
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                  )
+                ],
+              ),
             )
           ],
         ),
