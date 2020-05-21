20d60bd76cf290dbc62bc88d1463068069ee1d1a
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-21 21:54:18 +0430

    PetDetailsScreen add decoration to cover image

--- lib/screens/pet_details_screen.dart
+++ lib/screens/pet_details_screen.dart
@@ -9,12 +9,16 @@ class _PetDetailsScreenState extends State<PetDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Image.asset("assets/dog_cover.png"),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              child: Image.asset("assets/dog_cover.png"),
+              borderRadius: BorderRadius.only(bottomRight: Radius.circular(64)),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
