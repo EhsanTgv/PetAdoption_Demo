8f63aa04a986a2fc7e2c1c4097ba480ef540ec3c
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-21 21:43:46 +0430

    Implement PetDetailsScreen class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:petadoption/screens/home_screen.dart';
+import 'package:petadoption/screens/pet_details_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: PetDetailsScreen(),
     );
   }
 }
---
+++ lib/screens/pet_details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PetDetailsScreen extends StatefulWidget {
+  @override
+  _PetDetailsScreenState createState() => _PetDetailsScreenState();
+}
+
+class _PetDetailsScreenState extends State<PetDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
