420bd2d3e4b3abdb59f956304efb77500ec9d510
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-05-25 18:06:19 +0430

    implement colors.dart

---
+++ lib/config/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final Color blackColor = Color(0xFF08080D);
+final Color backgroundGrey = Color(0xFFEBEAF0);
+final Color textGrey = Color(0xFFB8B7BD);
+final Color accentRed = Color(0xFFF62E65);
